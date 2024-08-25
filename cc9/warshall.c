186f850b1da6a869254e1aef56a0a65900076b7e
MdSarfaraj1 <mahammadsarfaraj6@gmail.com>
2024-08-25 17:14:07 +0530

    Update warshall.c

--- cc9/warshall.c
+++ cc9/warshall.c
@@ -1,45 +1,70 @@
-#include<stdio.h>
-#define min(a,b) ((a)<(b)?(a):(b))
-int main()
-{
-int g[6][6]={
-    {0,2,2,999,999,999},
-    {999,0,999,2,3,999},
-    {999,1,0,999,3,4},
-    {999,999,999,0,999,2},
-    {999,999,999,999,0,1},
-    {999,999,999,999,999,0}
-};
-int i,j,k;
+#include <stdio.h>
+#include <limits.h>
 
-for(j=0;j<6;j++)
-{
-	for(i=0;i<6;i++){
-		printf("%d   ",g[j][i]);
-        }
-}
-// warshall
-for(k=0;k<6;k++)
-{
-    for(i=0;i<6;i++)
-    {
-        for(j=0;j<6;j++)
-        {
-           if( g[i][j]>( g[i][k] + g[k][j]))
-           {
-            g[i][j] = g[i][k] + g[k][j];
-              
-           }
+#define V 4 // Number of vertices in the graph
+#define INF INT_MAX // Representing infinity
+
+void printSolution(int dist[V][V], int next[V][V]) {
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (i != j) {
+                printf("Shortest path from vertex %d to vertex %d: ", i, j);
+                if (dist[i][j] == INF) {
+                    printf("No path\n");
+                } else {
+                    printf("%d ", i);
+                    int k = i;
+                    while (k != j) {
+                        k = next[k][j];
+                        printf("-> %d ", k);
+                    }
+                    printf(" | Cost: %d\n", dist[i][j]);
+                }
+            }
         }
     }
 }
-printf(" \n\n\nthe result is :");
-for(j=0;j<6;j++)
-{
-	for(i=0;i<6;i++){
-		printf("%d   ",g[j][i]);
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V], next[V][V];
+
+    // Initialize the distance and next matrices
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (graph[i][j] != INF && i != j) {
+                dist[i][j] = graph[i][j];
+                next[i][j] = j;
+            } else {
+                dist[i][j] = INF;
+                next[i][j] = -1;
+            }
         }
-        printf("\n");
+    }
+
+    // Floyd-Warshall algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    next[i][j] = next[i][k];
+                }
+            }
+        }
+    }
+
+    printSolution(dist, next);
 }
 
+int main() {
+    int graph[V][V] = {
+        {0, 3, INF, 7},
+        {8, 0, 2, INF},
+        {5, INF, 0, 1},
+        {2, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
 }
